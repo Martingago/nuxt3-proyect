3ff427c39b6aaad0aa4c83db10fe182a4829e052
Martin <gagochorenmartin@gmail.com>
2023-09-09 22:14:57 +0200

    ajuste en el formulario de imagen de portada (muestra previsualizacion de la imagen) añadidos algunos apartados en el componente de form de producto

--- components/forms/uploadImage.vue
+++ components/forms/uploadImage.vue
@@ -1,7 +1,8 @@
 <template>
     <form @submit.prevent="subirFichero" enctype="multipart/form-data">
-        <label for="uploadImage">Cargar imágenes</label>
+        <label for="uploadImage">Cargar imágen de portada</label>
         <input @change="clickImage($event)" type="file" accept="image/*" name="uploadImage" id="uploadImage">
+        <img @click="eliminarImagen" class="preview-image" v-if="imageURL" :src="imageURL" alt="imagen de portada de la aplicación">
         <button class="btn btn-primary" @click="subirFichero" :disabled="!imagen" type="button"> Subir imagen</button>
     </form>
 </template>
@@ -14,15 +15,37 @@
 //propiedades:
 const imagen = ref(null);
 const idProducto = ref('');
+const imageURL = ref(null)
 
 const clickImage = (e) => {
     imagen.value = e.target.files[0];
     idProducto.value = 'Ref34sdEf345dfdDsdsCD3DS234';
+    let reader = new FileReader();
+        reader.onloadend = function() {
+            imageURL.value = reader.result;
+        }
+        reader.readAsDataURL(imagen.value);
     console.log("archivo: ", imagen.value)
 }
+
+const eliminarImagen = () => {
+    imageURL.value = null;
+    imagen.value = null;
+}
+
+
 
 const subirFichero = async () => {
     await subirFicheroPortada(idProducto.value, imagen.value);
 }
 
 </script>
+
+<style scoped> 
+.preview-image{
+    width: 120px;
+    height: 120px;
+    background-color: lightgray;
+    object-fit: contain;
+}
+</style>
--- components/forms/uploadProduct.vue
+++ components/forms/uploadProduct.vue
@@ -7,8 +7,10 @@
         </div>
         <div class="d-flex flex-column">
             <label for="etiquetaProducto">Etiqueta del producto</label>
-            <select name="etiquetaProducto" id="etiquetaProducto">
-                <option value="ordenador">ordenador</option>
+            <select class="form-select" aria-label="Formulario de selección múltiple de clases de producto"
+                name="etiquetaProducto" id="etiquetaProducto" multiple>
+                <option v-for="categoria in dataCategorias" :key="categoria" :value="categoria.id">{{
+                    categoria.nombre_categoria }}</option>
             </select>
         </div>
         <div class="d-flex flex-column">
@@ -16,11 +18,21 @@
             <textarea id="descripcionProducto" cols="1" rows="4"></textarea>
         </div>
         <!-- Precios -->
+        <div class="alert alert-warning">
+            <p>Atención! Ingrese el precio del producto en el siguiente formato:</p>  
+            <p>- Utilice "." para separar los decimales. <br>
+                - No incluya ningún carácter para las unidades de millar.</p>
+            
+                
+    
+                <p>Ejemplo válido: 499.99 (para cuatrocientos noventa y nueve con noventa y nueve céntimos)</p>
+        </div>
         <div class="d-flex justify-content-between">
             <div class="d-flex flex-column">
                 <label for="precioProducto">Precio venta (PVP)</label>
                 <span class="d-flex flex-row justify-content-center align-items-center gap-1">
-                    <input type="text" @keypress="filterNonNumeric" name="precioProducto" id="precioProducto" class="text-end" v-model="precioProducto">
+                    <input type="text" @keypress="filterNonNumeric" name="precioProducto" id="precioProducto"
+                        class="text-end" v-model="precioProducto">
                     <p class="m-auto fw-600 fs-5">€</p>
                 </span>
             </div>
@@ -28,7 +40,8 @@
             <div class="d-flex gap-2">
                 <div class="d-flex flex-column">
                     <label for="descuentoProducto">Inluye descuento</label>
-                    <select name="decuentoProducto" id="descuentoProducto">
+                    <select v-model="descuentoProducto" @change="hasDiscount" name="decuentoProducto"
+                        id="descuentoProducto">
                         <option value="false">NO</option>
                         <option value="true"> SI</option>
                     </select>
@@ -38,18 +51,20 @@
                         <label for="porcentajeDescuento">Porcentaje de descuento</label>
                         <span class="d-flex gap-1 fw-700">
                             <input type="number" name="porcentajeDescuento" id="precioProducto"
-                            v-model="porcentajeDescuento" placeholder="25">
+                                v-model="porcentajeDescuento" placeholder="25">
                             <p class="m-auto fs-5 fw-600">%</p>
                         </span>
                     </div>
                     <div class="d-flex flex-column">
                         <label for="precioAnterior">Precio anterior</label>
-                        <span class="d-flex gap-1"><input v-model="precioAnterior" type="number" name="precioAnterior" id="precioAnterior">
-                        <p class="m-auto fs-5 fw-600">€</p>
+                        <span class="d-flex gap-1"><input v-model="precioAnterior" type="number" name="precioAnterior"
+                                id="precioAnterior">
+                            <p class="m-auto fs-5 fw-600">€</p>
                         </span>
                     </div>
 
-                    <button class="btn btn-primary p-0 mt-2" @click="calcularPVP">Calcular PVP</button>
+                    <button :disabled="!booleanDiscount" class="btn btn-primary p-0 mt-2" @click="calcularPVP">Calcular
+                        PVP</button>
                 </div>
             </div>
 
@@ -67,23 +82,39 @@
 
 const precioProducto = ref();
 const porcentajeDescuento = ref();
-const precioAnterior = ref()
+const precioAnterior = ref();
+
+//obtener datos de las categorias:
+const dataCategorias = ref([]);
+
+//determinar si tiene descuento o no:
+
+const descuentoProducto = ref('false');
+const booleanDiscount = ref(false)
+const hasDiscount = () => {
+    booleanDiscount.value = descuentoProducto.value === 'true';
+}
+
+
+onMounted(async () => {
+    dataCategorias.value = await getDataFromStore('categoria_productos');
+})
+
 
 const calcularPVP = () => {
     const descuento = 1 - porcentajeDescuento.value / 100;
     const calculoDescuento = descuento * precioAnterior.value;
-    precioProducto.value = calculoDescuento; 
+    precioProducto.value = calculoDescuento;
     console.log(precioProducto.value)
 }
 
-const filterNonNumeric = (event) =>{
-      // Verificar si el valor ingresado es numérico
-      if (!/\d/.test(event.key) && event.key !== '.') {
+const filterNonNumeric = (event) => {
+    // Verificar si el valor ingresado es numérico
+    if (!/\d/.test(event.key) && event.key !== '.') {
         // Si el valor no es numérico, evitar que se ingrese en el input
         event.preventDefault();
-      }
     }
-
+}
 
 </script>
 
@@ -93,11 +124,10 @@ form {
     max-width: 800px;
 }
 
-.fw-600{
+.fw-600 {
     font-weight: 600;
 }
 
 #descripcionProducto {
     resize: none;
-}
-</style>
+}</style>
