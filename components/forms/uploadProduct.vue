f8d7358329623a58daea8f8b6743da031c4abe2c
Martin <gagochorenmartin@gmail.com>
2023-09-10 18:35:56 +0200

    cambios en el formulario de uploadProduct -añadidos avisos en actualizacion de precios -objeto con los datos del formulario

--- components/forms/uploadProduct.vue
+++ components/forms/uploadProduct.vue
@@ -3,7 +3,8 @@
         <h3 class=" text-center">Añadir producto</h3>
         <div class="d-flex flex-column">
             <label for="nombreProducto">Nombre del producto</label>
-            <input type="text" name="nombre" id="nombreProducto" placeholder="Nombre del producto">
+            <input type="text" required v-model="datos_articulo.nombre_articulo" name="nombre" id="nombreProducto"
+                placeholder="Nombre del producto">
         </div>
         <div class="d-flex flex-column">
             <label for="etiquetaProducto">Etiqueta del producto</label>
@@ -15,32 +16,31 @@
         </div>
         <div class="d-flex flex-column">
             <label for="descripcionProducto">Descripción del producto</label>
-            <textarea id="descripcionProducto" cols="1" rows="4"></textarea>
+            <textarea id="descripcionProducto" required cols="1" rows="4" v-model="datos_articulo.descripcion_articulo"></textarea>
+        </div>
+        
+        <div class="alert alert-warning">
+            <p>Atención! Ingrese el precio del producto en el siguiente formato:</p>
+            <p>- Utilice "." o "," para separar los decimales. <br>
+                - No incluya ningún carácter para las unidades de millar.</p>
+            <p>Ejemplo válido: 499.99 (para cuatrocientos noventa y nueve con noventa y nueve céntimos) <br>
+            Ejemplo válido: 1499,99 (para mil cuatrocientos noventa y nueve con noventa y nueve céntimos)</p>
         </div>
         <!-- Precios -->
-        <div class="alert alert-warning">
-            <p>Atención! Ingrese el precio del producto en el siguiente formato:</p>  
-            <p>- Utilice "." para separar los decimales. <br>
-                - No incluya ningún carácter para las unidades de millar.</p>
-            
-                
-    
-                <p>Ejemplo válido: 499.99 (para cuatrocientos noventa y nueve con noventa y nueve céntimos)</p>
-        </div>
         <div class="d-flex justify-content-between">
             <div class="d-flex flex-column">
                 <label for="precioProducto">Precio venta (PVP)</label>
-                <span class="d-flex flex-row justify-content-center align-items-center gap-1">
-                    <input type="text" @keypress="filterNonNumeric" name="precioProducto" id="precioProducto"
-                        class="text-end" v-model="precioProducto">
-                    <p class="m-auto fw-600 fs-5">€</p>
+                <span class="d-flex flex-row justify-content-left align-items-left gap-1">
+                    <input type="number" step="0.01"  name="precioProducto" id="precioProducto"
+                        class="text-end" required   v-model="datos_articulo.precio_venta" @input="checkAlert">
+                    <p class="m-left my-auto fw-600 fs-5">€</p>
                 </span>
             </div>
 
             <div class="d-flex gap-2">
                 <div class="d-flex flex-column">
                     <label for="descuentoProducto">Inluye descuento</label>
-                    <select v-model="descuentoProducto" @change="hasDiscount" name="decuentoProducto"
+                    <select  @change="hasDiscount($event)" name="decuentoProducto"
                         id="descuentoProducto">
                         <option value="false">NO</option>
                         <option value="true"> SI</option>
@@ -51,70 +51,110 @@
                         <label for="porcentajeDescuento">Porcentaje de descuento</label>
                         <span class="d-flex gap-1 fw-700">
                             <input type="number" name="porcentajeDescuento" id="precioProducto"
-                                v-model="porcentajeDescuento" placeholder="25">
+                                v-model="datos_articulo.porcentaje_descuento" :required="datos_articulo.descuento" :disabled="!datos_articulo.descuento">
                             <p class="m-auto fs-5 fw-600">%</p>
                         </span>
                     </div>
                     <div class="d-flex flex-column">
                         <label for="precioAnterior">Precio anterior</label>
-                        <span class="d-flex gap-1"><input v-model="precioAnterior" type="number" name="precioAnterior"
+                        <span class="d-flex gap-1"><input v-model="datos_articulo.precio_anterior" :required="datos_articulo.descuento" :disabled="!datos_articulo.descuento" type="number" step="0.01" name="precioAnterior"
                                 id="precioAnterior">
                             <p class="m-auto fs-5 fw-600">€</p>
                         </span>
                     </div>
 
-                    <button :disabled="!booleanDiscount" class="btn btn-primary p-0 mt-2" @click="calcularPVP">Calcular
+                    <button :disabled="!datos_articulo.descuento" class="btn btn-primary p-0 mt-2" @click.prevent="calcularPVP">Calcular
                         PVP</button>
                 </div>
+                
             </div>
-
+            
+        </div>
+        <div class="notification-area rounded">
+            <p v-if="alerta" class="alert alert-danger m-0 p-1">Error! El precio no puede ser menor o igual a 0</p>
+            <p v-if="alertaUpdt" class="alert alert-warning m-0 p-1">Atencion! El precio del producto se ha actualizado de <span class="fw-bold">{{ pvpDesfase }}€</span>  a <span class="fw-bold">{{datos_articulo.precio_venta}}€</span> en base a los descuentos aplicados</p>
         </div>
 
         <div class="d-flex flex-column">
             <label for="stockProducto">Stock del artículo</label>
-            <input type="number" name="stockProducto" id="stockProducto">
+            <input type="number" required name="stockProducto" id="stockProducto">
         </div>
-
+        <button @click="subirProducto" class="btn btn-primary">Subir producto</button>
     </form>
 </template>
 
 <script setup>
-
-const precioProducto = ref();
-const porcentajeDescuento = ref();
-const precioAnterior = ref();
-
-//obtener datos de las categorias:
-const dataCategorias = ref([]);
-
-//determinar si tiene descuento o no:
-
-const descuentoProducto = ref('false');
-const booleanDiscount = ref(false)
-const hasDiscount = () => {
-    booleanDiscount.value = descuentoProducto.value === 'true';
-}
-
-
 onMounted(async () => {
     dataCategorias.value = await getDataFromStore('categoria_productos');
 })
 
 
-const calcularPVP = () => {
-    const descuento = 1 - porcentajeDescuento.value / 100;
-    const calculoDescuento = descuento * precioAnterior.value;
-    precioProducto.value = calculoDescuento;
-    console.log(precioProducto.value)
-}
+//obtener datos de las categorias:
+const dataCategorias = ref([]);
+const alerta = ref(false);
+const alertaUpdt = ref(false);
+const pvpDesfase = ref(0);
 
-const filterNonNumeric = (event) => {
-    // Verificar si el valor ingresado es numérico
-    if (!/\d/.test(event.key) && event.key !== '.') {
-        // Si el valor no es numérico, evitar que se ingrese en el input
-        event.preventDefault();
+const datos_articulo = ref({
+    nombre_articulo: "",
+    etiquetas_articulo: [],
+    descripcion_articulo: "",
+    precio_venta: null,
+    descuento: null,
+    porcentaje_descuento: null,
+    precio_anterior: null,
+    stock_articulo: null
+})
+
+
+//determinar si tiene descuento o no:
+const hasDiscount = (event) => {
+    datos_articulo.value.descuento = event.target.value === 'true';
+    if(event.target.value === 'false'){
+        datos_articulo.value.porcentaje_descuento = null;
+        datos_articulo.value.precio_anterior = null;
     }
 }
+
+const calcularPVP = () => {
+    if(datos_articulo.value.porcentaje_descuento >= 1 && datos_articulo.value.porcentaje_descuento <100){
+        const descuento = 1 - datos_articulo.value.porcentaje_descuento / 100;
+    const calculoDescuento = Number(descuento * datos_articulo.value.precio_anterior).toFixed(2);
+        //Notificar de que el precio de producto se ha cambiado:
+        pvpDesfase.value = datos_articulo.value.precio_venta
+    if(datos_articulo.value.precio_venta != calculoDescuento){
+        console.log(`el precio se ha actualizado de: ${pvpDesfase.value} a ${calculoDescuento}`)
+        alertaUpdt.value = true;
+    }
+
+    datos_articulo.value.precio_venta = calculoDescuento;
+    }
+}
+
+const checkAlert = () => {
+    if(datos_articulo.value.precio_venta <=0){
+        alerta.value = true
+    }else{
+        alertaUpdt.value = false
+        alerta.value = false
+    }
+}
+
+const subirProducto = () => {
+    //salta alerta de error de precio
+    if(datos_articulo.value.precio_venta <1){
+        alerta.value = true;
+    }else{
+        //se produce un update en el precio en caso de que no se haya producido antes
+        calcularPVP();
+        if(alertaUpdt.value == true){
+            console.log("el producto se subirá con la siguiente alerta: Actualización de precio" )
+        }
+        console.log(datos_articulo.value)
+    }
+    
+}
+
 
 </script>
 
@@ -130,4 +170,16 @@ form {
 
 #descripcionProducto {
     resize: none;
-}</style>
+}
+
+
+.notification-area{
+    width: 100%;
+    height: 34px;
+    background-color: rgba(247, 246, 246, 0.856);
+}
+
+.fw-bold{
+    text-decoration: bold;
+}
+</style>
